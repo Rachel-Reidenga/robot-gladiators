bc0d992edbfff36ac7425e8e130de8b3cf8880cf
Rachel <63015606+Rachel-Reidenga@users.noreply.github.com>
2020-12-16 20:33:41 -0600

    initial js

---
+++ games.js
@@ -0,0 +1,70 @@
+// var playerName = window.prompt("What is your robot's name?");
+
+// var playerName = window.prompt("What is your robot's name?");
+
+// console.log()
+
+// console.log("This logs a string, good for leaving yourself a message");
+// // this will do math and log 20
+// console.log(10 + 10);
+// // what is this?
+// console.log("Our robot's name is " + playerName);
+
+// var name = "your name";
+// console.log(name);
+
+// console.log("This logs a string, good for leaving yourself a message");
+// // this will do math and log 20
+// console.log(10 + 10);
+// // what is this?
+// var playerName = window.prompt("Our robot's name is " + playerName);
+// //What's is this?
+// console.log(playerName);
+
+// window.alert("This is an alert! JavaScript is running!");
+
+// function fight() {
+//     window.alert("The fight has begun!");
+//   }
+
+  // fight();
+
+  //replace everything with the following
+
+  var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+  // Log a resulting message to the console so we know that it worked.
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+  // Log a resulting message to the console so we know that it worked.
+};
+var fight = function() {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+  // Log a resulting message to the console so we know that it worked.
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+  // Log a resulting message to the console so we know that it worked.
+};
+
+fight();
